73c2b7798a7fe7767e1c697da689eee764e6790d
skalas <miguel@opi.la>
2015-02-11 00:55:02 -0600

    Jala los otros servidores.

---
+++ packages.el
@@ -0,0 +1,7 @@
+; packages.el
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
