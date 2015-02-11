7ea37387f0e506bfa66190741f8a7c2532eabc3b
skalas <miguel@opi.la>
2015-02-11 01:40:25 -0600

    cambios que avanzan el proyecto

--- configs.el
+++ configs.el
@@ -1,3 +1,13 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; ido mode (muy útil para la navegación de lo de abajo)
+(ido-mode t)
+;; Prendemos el autocomplete.
+(auto-complete-mode t)
+(global-auto-complete-mode t)
+(electric-pair-mode 1)
+(require 'iso-transl)
+
+(setq tab-width 4)
--- init.el
+++ init.el
@@ -1,1 +1,5 @@
 ;; Archivo que jalará todo lo demás.
+(load "~/.emacs.d/packages.el")
+(load "~/.emacs.d/configs.el")
+(load "~/.emacs.d/aliases.el")
+
