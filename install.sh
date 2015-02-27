c5f1c027aaea255e8f8cf0806e78da23fce7635c
skalas <miguel@opi.la>
2015-02-26 19:50:35 -0600

    Agrego paquetes para que orgmode y ox-reveal funcionen.

--- install.sh
+++ install.sh
@@ -10,6 +10,16 @@ do
     cp $i ~/.emacs.d/
 done
 cd ~/.emacs.d
+## instalamos el nav-bar
 wget https://emacs-nav.googlecode.com/files/emacs-nav-49.tar.gz
 tar -zxvf emacs-nav-49.tar.gz
-
+## instalo org-mode
+mkdir paquetes
+cd paquetes
+git clone git://orgmode.org/org-mode.git && cd org-mode && git checkout maint &&make autoloads && cd .. 
+echo '========================================================='
+echo '=====================org-mode============================'
+echo '========================================================='
+git clone https://github.com/yjwen/org-reveal.git && cd org-reveal && git checkout stable && cp ox-reveal.el ../org-mode/lisp/ && cd ..
+## instalamos reveal.js
+sudo chown -R "$USER:$USER" ~/.emacs.d
