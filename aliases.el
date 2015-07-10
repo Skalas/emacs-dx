f602a2dac272d45882b0d0df93730586204ed680
skalas <miguel@opi.la>
2015-07-10 16:26:00 -0500

    Cambios a que se parezca más a tmux

--- aliases.el
+++ aliases.el
@@ -3,10 +3,10 @@
 ;; para comentar regiones en pocas lineas
 (global-set-key "\C-x\C-b" 'ibuffer)
 (global-set-key "\C-xg" 'magit-status)	
-(global-set-key (kbd "<S-M-up>") 'window-jump-up) 
-(global-set-key (kbd "<S-M-right>") 'window-jump-right)
-(global-set-key (kbd "<S-M-down>") 'window-jump-down)
-(global-set-key (kbd "<S-M-left>") 'window-jump-left)
+(global-set-key (kbd "<M-up>") 'window-jump-up) 
+(global-set-key (kbd "<M-right>") 'window-jump-right)
+(global-set-key (kbd "<M-down>") 'window-jump-down)
+(global-set-key (kbd "<M-left>") 'window-jump-left)
 ;; para aumento y decremento de zoom
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
