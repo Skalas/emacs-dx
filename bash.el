490cd1743adc5906e33f4c92945d69420374eab3
Miguel Escalante <escalas5@gmail.com>
2015-11-11 19:22:45 -0600

    bash changes

--- bash.el
+++ bash.el
@@ -10,4 +10,11 @@
   (define-key sh-mode-map "\C-c\C-d" 'shell-cd-current-directory)) 
 (add-hook 'sh-mode-hook 'essh-sh-hook)                             
 
+(global-set-key [f1] 'shell)
 
+(setq explicit-shell-file-name "/bin/bash")
+(setq shell-file-name "bash")
+(setq explicit-bash-args '("--noediting" "--login" "-i"))
+(setenv "SHELL" shell-file-name)
+
+(add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
