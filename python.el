2e4a71bcf66b58e570dadd5c5c1335ae3d4b4d74
Miguel Escalante <escalas5@gmail.com>
2015-11-18 17:17:57 -0600

    CAmbio de config a python, esta me gustó más :)

--- python.el
+++ python.el
@@ -1,2 +1,10 @@
-(package-initialize)
-(elpy-enable)
+(require 'python-mode)
+
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+;; (setq py-shell-switch-buffers-on-execute-p t)
+;; (setq py-switch-buffers-on-execute-p t)
+(setq py-split-windows-on-execute-p t)
