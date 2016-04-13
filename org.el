6a43db5d606c0bca676f7b93ecd915aa433e0a0a
Miguel Escalante <escalas5@gmail.com>
2016-04-12 19:41:56 -0500

    org languajes0

--- org.el
+++ org.el
@@ -1,3 +1,14 @@
 (add-to-list 'load-path "~/.emacs.d/paquetes/org-mode/lisp")
+
 (require 'ox-reveal)
 (setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/2.5.0/")
+(custom-set-variables
+ '(org-babel-load-languages
+   (quote (
+	   (emacs-lisp . t)
+	   (R . t)
+	   (python . t)
+	   (bash . t)
+	   ))
+   )
+ '(org-confirm-babel-evaluate nil))
