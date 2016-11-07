bd3647ce77adff034bddf981984e7222d77ce213
Miguel Escalante <escalas5@gmail.com>
2016-11-07 13:05:28 -0600

    changes

--- python.el
+++ python.el
@@ -1,10 +1,12 @@
 (require 'python-mode)
-
-(setq-default py-shell-name "ipython")
+(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
+(setq-default py-shell-name "/home/skalas/.pyenv/shims/ipython")
 (setq-default py-which-bufname "IPython")
 (setq py-python-command-args
-  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+      '("--gui=wx" "--pylab=wx" "--simple-prompt" "-i"))
 (setq py-force-py-shell-name-p t)
 ;; (setq py-shell-switch-buffers-on-execute-p t)
 ;; (setq py-switch-buffers-on-execute-p t)
 (setq py-split-windows-on-execute-p t)
+(setq py-python-command-args '("--colors=linux"))
+
