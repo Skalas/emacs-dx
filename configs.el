c3604747e1bad57601d56669b3606f4a7ee182ff
Miguel Escalante <escalas5@gmail.com>
2017-03-14 23:58:13 -0600

    python, finally

--- configs.el
+++ configs.el
@@ -1,11 +1,11 @@
+
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 ;; ido mode (muy útil para la navegación de lo de abajo)
 (ido-mode t)
 ;; Prendemos el autocomplete.
-(auto-complete-mode t)
-(global-auto-complete-mode t)
+
 (electric-pair-mode 1)
 (require 'iso-transl)
 (setq tab-width 4)
@@ -25,3 +25,5 @@
 (show-paren-mode t)
 
 (yas-global-mode 1)
+(auto-complete-mode t)
+(global-auto-complete-mode t)
--- python.el
+++ python.el
@@ -1,12 +1,32 @@
-(require 'python-mode)
-(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
-(setq-default py-shell-name "/home/skalas/.pyenv/shims/ipython")
-(setq-default py-which-bufname "IPython")
-(setq py-python-command-args
-      '("--gui=wx" "--pylab=wx" "--simple-prompt" "-i"))
-(setq py-force-py-shell-name-p t)
-;; (setq py-shell-switch-buffers-on-execute-p t)
-;; (setq py-switch-buffers-on-execute-p t)
-(setq py-split-windows-on-execute-p t)
-(setq py-python-command-args '("--colors=linux"))
+;; (require 'python-mode)
+;; (setenv "IPY_TEST_SIMPLE_PROMPT" "1")
+;; (setq-default py-shell-name "/home/skalas/.pyenv/shims/ipython")
+;; (setq-default py-which-bufname "IPython")
+;; (setq py-python-command-args
+;;       '("--gui=wx" "--pylab=wx" "--simple-prompt" "-i"))
+;; (setq py-force-py-shell-name-p t)
+;; ;; (setq py-shell-switch-buffers-on-execute-p t)
+;; ;; (setq py-switch-buffers-on-execute-p t)
+;; (setq py-split-windows-on-execute-p t)
+;; (setq py-python-command-args '("--colors=linux"))
 
+;;(exec-path-from-shell-copy-env "PATH")
+
+;; PYTHON CONFIGURATION
+;; --------------------------------------
+
+
+(elpy-enable)
+(elpy-use-ipython)
+(require 'pyenv-mode-auto)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "--simple-prompt --pprint")
+;; use flycheck not flymake with elpy
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; enable autopep8 formatting on save
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(add-to-list 'exec-path "$HOME//.pyenv/shims/")
