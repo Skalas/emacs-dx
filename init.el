fbfe4731894492981c170d84944f51cbe1412ab4
Miguel Escalante <escalas5@gmail.com>
2017-04-12 13:22:26 -0500

    emacs-dx

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 
 ;; Archivo que jalará todo lo demás.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (load "~/.emacs.d/packages.el")
 (load "~/.emacs.d/configs.el")
 (load "~/.emacs.d/aliases.el")
--- python.el
+++ python.el
@@ -15,12 +15,12 @@
 ;; PYTHON CONFIGURATION
 ;; --------------------------------------
 
-
+;;(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
 (elpy-enable)
-(elpy-use-ipython)
+;;(elpy-use-ipython)
 (require 'pyenv-mode-auto)
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "--simple-prompt --pprint")
+;; (setq python-shell-interpreter "ipython"
+;;       python-shell-interpreter-args "--gui=wx --pylab=wx --simple-prompt -i")
 ;; use flycheck not flymake with elpy
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
@@ -30,3 +30,13 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 (add-to-list 'exec-path "$HOME//.pyenv/shims/")
+
+(with-eval-after-load 'python
+  (defun python-shell-completion-native-try ()
+    "Return non-nil if can trigger native completion."
+    (let ((python-shell-completion-native-enable t)
+          (python-shell-completion-native-output-timeout
+           python-shell-completion-native-try-output-timeout))
+      (python-shell-completion-native-get-completions
+       (get-buffer-process (current-buffer))
+       nil "_"))))
