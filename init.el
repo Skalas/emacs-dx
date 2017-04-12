8c1e7459b2c1a3f1bf4d532baa639064344015ed
Miguel Escalante <escalas5@gmail.com>
2017-04-12 18:35:11 -0500

    weird

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;;(package-initialize)
 
 (load "~/.emacs.d/packages.el")
 (load "~/.emacs.d/configs.el")
