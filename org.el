3ed7727af9ff16dacabc94ec62a99b13f15504b8
Miguel Escalante <escalas5@gmail.com>
2020-02-06 14:12:06 -0600

    new stuff

--- org.el
+++ org.el
@@ -1,35 +1,23 @@
-(if (package-installed-p 'org)
-  (setq org-install nil)
-  (setq org-install t)
-  )
-
-
-(use-package htmlize
-  :ensure t)
-
-
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (use-package org
-  :ensure org-plus-contrib
   :mode ("\\.org\\'" . org-mode)
+  :ensure org-plus-contrib
   :config
   (progn
     ;; Org-babel
     ;; No preguntar para confirmar la evaluación
     (setq org-confirm-babel-evaluate nil)
-
-     (org-babel-do-load-languages
-      'org-babel-load-languages
-      '(
-	(emacs-lisp . t)
-	(R . t)
-	(python . t)
-	(sql . t)
-	(shell . t)
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '(
+       (emacs-lisp . t)
+       (R . t)
+       (python . t)
+       (sql . t)
+       (shell . t)
 	))
-      ) ;; Fin de progn
-  :pin org
-  )  ;; Fin de use-package org
+    ) ;; Fin de progn
+  )
 
 (use-package ox-reveal
   :ensure t
@@ -38,31 +26,34 @@
 	(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/2.5.0/"))
   )
 
+(use-package htmlize
+  :ensure t
+  :no-require t)
 
 ;;(setq org-agenda-files '("~/Dropbox/org/"))
 
-(use-package org-bullets
-  :ensure t
-  :commands (org-bullets-mode)
-  :init (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+;; (use-package org-bullets
+;;   :ensure t
+;;   :commands (org-bullets-mode)
+;;   :init (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 ;; (setq org-todo-keywords '((sequence "☛ TODO(t)" "|" "<img draggable="false" class="emoji" alt="✔" src="https://s0.wp.com/wp-content/mu-plugins/wpcom-smileys/twemoji/2/svg/2714.svg" scale="0"> DONE(d)")
 ;; (sequence "⚑ WAITING(w)" "|")
 ;; (sequence "|" "✘ CANCELED(c)")))
 
-(setq org-log-done 'time)
+;; (setq org-log-done 'time)
 
-(if (boundp org-install)
-    (message "I installed org")
-  nil
-  )
+;; (if (boundp org-install)
+;;     (message "I installed org")
+;;   nil
+;;   )
 
-(if (boundp org-install)
-    (byte-recompile-file
-     (expand-file-name "ob-R.el"
-		       (file-name-directory (locate-library "org")))
-     t)
-  nil
-  )
+;; (if (boundp org-install)
+;;     (byte-recompile-file
+;;      (expand-file-name "ob-R.el"
+;; 		       (file-name-directory (locate-library "org")))
+;;      t)
+;;   nil
+;;   )
 
 
