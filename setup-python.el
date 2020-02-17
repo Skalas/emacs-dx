3e329153dacd981ba50c8ef3c1f4ae59417173ed
Miguel Escalante <escalas5@gmail.com>
2020-02-17 13:10:06 -0600

    new version

--- setup-python.el
+++ setup-python.el
@@ -7,11 +7,15 @@
   :ensure t
   :config
   (elpy-enable)
-  (pyvenv-activate "~/src/anaconda3")
-  (setq python-shell-interpreter "python3"
-      python-shell-interpreter-args "-i"
-      elpy-rpc-python-command "python3")
+  (pyvenv-activate "~/src/miniconda3")
+  (setq
+   python-shell-interpreter "python3"
+   python-shell-interpreter-args "-i"
+   elpy-rpc-python-command "python3"
+   elpy-shell-starting-directory 'current-directory
+   )
   (delete `elpy-module-django elpy-modules)
+  (setenv "WORKON_HOME" "~/src/miniconda3/envs")
   (delete `elpy-module-highlight-indentation elpy-modules))
 
 (use-package py-autopep8
@@ -21,7 +25,6 @@
   :config
   (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
 
-(setenv "WORKON_HOME" "~/src/anaconda3/envs")
 
 
 (provide 'setup-python)
