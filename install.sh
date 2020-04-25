87c372bce02c49617cad378a7740a4cf6def41fb
Miguel Escalante <escalas5@gmail.com>
2020-04-25 14:16:25 -0500

    Changes to the org package

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ do
     echo "$i"
     cp $i ~/.emacs.d/
 done
-cd ~/.emacs.d
+cp configs.org ~/.emacs.d/configs.org
+#cd ~/.emacs.d
 ## instalamos el nav-bar
-wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/emacs-nav/emacs-nav-49.tar.gz
-tar -zxvf emacs-nav-49.tar.gz
+#wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/emacs-nav/emacs-nav-49.tar.gz
+#tar -zxvf emacs-nav-49.tar.gz
